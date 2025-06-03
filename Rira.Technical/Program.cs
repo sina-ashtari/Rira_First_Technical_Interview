6843142ddf55780c1c3b96200972d77ecd685769
sina-ashtari <sinaa1383@gmail.com>
2025-06-03 17:11:10 +0330

    initial commit

---
+++ Rira.Technical/Program.cs
@@ -0,0 +1,31 @@
+﻿using Rira.Technical.Data;using Rira.Technical.Models;
+using Rira.Technical.Services;
+
+
+IProductService productService = new ProductService(StaticProductData.Products);
+
+var category1Products = productService.GetProductsByCategory(Categories.Category1);
+var mostExpensive = productService.GetMostExpensiveProduct();
+var totalPrice = productService.GetTotalPrice();
+var grouped = productService.GroupByCategory();
+var avgPrice = productService.GetAveragePrice();
+
+Console.WriteLine("Category 1 Products: ");
+foreach (var product in category1Products)
+{
+    Console.WriteLine("Name: " + product.Name + " with id: " + product.Id + ", category: " + product.Category + ", price: " + product.Price);
+}
+
+Console.WriteLine("Most expensive product is: " +  mostExpensive.Name + " with id: " + mostExpensive.Id + ", category: " + mostExpensive.Category + ", price: " + mostExpensive.Price);
+Console.WriteLine("Total price is: " + totalPrice);
+
+foreach (var category in grouped)
+{
+    Console.WriteLine($"{category.Key}: ");
+    foreach (var product in category.Value)
+    {
+        Console.WriteLine("Name: " + product.Name + " with id: " + product.Id + ", category: " + product.Category + ", price: " + product.Price);
+    }
+}
+
+Console.WriteLine("Average price: " + avgPrice);
