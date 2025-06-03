67d14c121bf4feb52bbb5ee93ef7fd4472de5d53
sina-ashtari <sinaa1383@gmail.com>
2025-06-03 17:17:13 +0330

    Feat : Add Product data class.

---
+++ Rira.Technical/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Rira.Technical.Models;
+
+public class Product
+{
+    public required int Id {get; init;}
+    public required string Name{get; init;}
+    public required Categories Category{get; init;}
+    public decimal Price{get; init;}
+}
