f439dab2bc66d764d74affe274870c587e626d86
sina-ashtari <sinaa1383@gmail.com>
2025-06-03 17:16:31 +0330

    Feat : Add Products service. Products related actions now available from this service.

---
+++ Rira.Technical/Services/IProductsService.cs
@@ -0,0 +1,12 @@
+﻿using Rira.Technical.Models;
+
+namespace Rira.Technical.Services;
+
+public interface IProductsService
+{
+    IEnumerable<Product> GetProductsByCategory(Categories category);
+    Product GetMostExpensiveProduct();
+    decimal GetTotalPrice();
+    Dictionary<Categories, List<Product>> GroupByCategory();
+    decimal GetAveragePrice();
+}
